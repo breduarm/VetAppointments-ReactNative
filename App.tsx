ccf38b9690072343cce0088a495c4c94b60f4538
breduarm <bryan.e.armijos@gmail.com>
2024-05-19 22:34:58 -0500

    Modify App.tsx file

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   Text,
+  StyleSheet,
+  Pressable,
 } from 'react-native';
 
 type SectionProps = PropsWithChildren<{
@@ -12,11 +14,53 @@ type SectionProps = PropsWithChildren<{
 
 const App = (): React.JSX.Element => {
 
+  const createAppHandler = () => {
+    console.log("Pressable onPress")
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Hello World !!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.h1}>Medical Appointment Manager {''}
+        <Text style={styles.h1Bold}>Veterinary</Text>
+      </Text>
+      <Pressable onPress={createAppHandler} style={styles.button}>
+        <Text style={styles.buttonText}>New Appointment</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1,
+  },
+  h1: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600',
+  },
+  h1Bold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+  },
+  button: {
+    backgroundColor: '#6D28D9',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 48,
+    marginTop: 20,
+    marginHorizontal: 24,
+    borderRadius: 8,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+  },
+})
 
 export default App;
