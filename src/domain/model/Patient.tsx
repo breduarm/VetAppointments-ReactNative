641f27f20b5e03453e4c091d12b2063279c38863
breduarm <bryan.e.armijos@gmail.com>
2024-06-04 01:08:05 -0500

    Add format coding to Patient

--- src/domain/model/Patient.tsx
+++ src/domain/model/Patient.tsx
@@ -1,20 +1,27 @@
 class Patient {
-    id: string;
-    name: string;
-    owner: string;
-    email: string;
-    phone: string;
-    dateApp: Date;
-    symptoms: string;
+  id: string;
+  name: string;
+  owner: string;
+  email: string;
+  phone: string;
+  dateApp: Date;
+  symptoms: string;
 
-    constructor (name: string, owner: string, email: string, phone: string, dateApp: Date, symptoms: string) {
-        this.name = name;
-        this.owner = owner;
-        this.email = email;
-        this.phone = phone;
-        this.dateApp = dateApp;
-        this.symptoms = symptoms;
-    }
+  constructor(
+    name: string,
+    owner: string,
+    email: string,
+    phone: string,
+    dateApp: Date,
+    symptoms: string,
+  ) {
+    this.name = name;
+    this.owner = owner;
+    this.email = email;
+    this.phone = phone;
+    this.dateApp = dateApp;
+    this.symptoms = symptoms;
+  }
 }
 
-export default Patient
+export default Patient;
